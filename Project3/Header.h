8e7b973216d3a44a5cc977a35c0f1269fe92c9eb
Feeld3r <Titan@DESKTOP>
2022-07-02 16:16:26 +0300

    Added discription to functions

--- Project3/Header.h
+++ Project3/Header.h
@@ -8,8 +8,18 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+* ќтриманн€ року, м≥с€ц€ випуску та номеру парт≥њ з сер≥йного номеру
+* @param x - число которое будет в степени
+* @param st - степень числа x
+* @return sum - сумма чисел числа
+*/
 long long sum_digits(int x, int st);
 
+/**
+* ќчищение строки от знаков после символа '\0'
+* @return 1 - корректна€ работа
+*/
 int clean_stdin();
 
 #endif // !PRACT_SUM
