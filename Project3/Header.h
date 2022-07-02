2dc311988cc1741db36399816ede356ef8b64252
Feeld3r <Titan@DESKTOP>
2022-07-02 16:21:05 +0300

    add

--- Project3/Header.h
+++ Project3/Header.h
@@ -8,12 +8,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-/**
-* ќтриманн€ року, м≥с€ц€ випуску та номеру парт≥њ з сер≥йного номеру
-* @param x - число которое будет в степени
-* @param st - степень числа x
-* @return sum - сумма чисел числа
-*/
+
 long long sum_digits(int x, int st);
 
 /**
